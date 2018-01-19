ca76514ce7f823395f8d29a078d5d552deb9c5d9
Gauthier Billot <gauthier.billot@fishingcactus.com>
2018-01-19 16:03:20 +0100

    Update

--- src/hpa/Transform.hx
+++ src/hpa/Transform.hx
@@ -4,7 +4,7 @@ class Transform
 {
     public var position:Point = new Point();
     public var rotation:Float;
-    public var scale:phaser.Point = new phaser.Point();
+    public var scale:Point = new Point();
 
     public function new(x = 0.0, y = 0.0, r = 0.0, sx = 1.0, sy = 1.0)
     {
