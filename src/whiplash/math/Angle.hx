59649781a6ca30ee7fc9523c629e630a410a14d0
Julien Hamaide <julien.hamaide@gmail.com>
2018-03-14 11:07:15 +0100

    Angle helper

---
+++ src/whiplash/math/Angle.hx
@@ -0,0 +1,18 @@
+package whiplash.math;
+
+class Angle {
+
+
+    public static function getSmallest(angle:Float):Float{
+
+        var result = angle % 360;
+
+        if( result > 180 ){
+            result -= 360;
+        } else if ( result < -180 ){
+            result += 360;
+        }
+
+        return result;
+    }
+}
