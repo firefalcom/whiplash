e44ff0bba8ba7beef7a8c0696bb0e5ff582224bc
Gauthier Billot <gauthier.billot@fishingcactus.com>
2018-04-08 14:11:00 +0200

    Added canvas var

--- src/whiplash/Lib.hx
+++ src/whiplash/Lib.hx
@@ -6,19 +6,20 @@ class Lib {
     static public var phaserGame:phaser.Game;
     static public var babylonEngine:babylonx.Engine;
     static public var ashEngine:ash.core.Engine;
+    static public var babylonCanvas:js.html.CanvasElement;
 
     static public function init(?width:Int = 800, ?height:Int = 600, ?parent:String = "body", ?callbacks:{?preload:Void->Void, ?create:Void->Void, ?update:Void->Void} = null, ?systemsPriority:Int = 10) {
         phaserGame = new phaser.Game(width, height, phaser.Phaser.CANVAS, '', {preload:callbacks.preload, create:callbacks.create, update:callbacks.update});
         phaserGame.transparent = true;
-        var canvas = js.Browser.document.createCanvasElement();
-        document.querySelector(parent).appendChild(canvas);
-        canvas.width = width;
-        canvas.height = height;
-        canvas.style.width = width+"px";
-        canvas.style.height = height+"px";
-        canvas.style.zIndex = "-1";
-        canvas.style.position = "absolute";
-        babylonEngine = new babylonx.Engine(canvas, true);
+        babylonCanvas = js.Browser.document.createCanvasElement();
+        document.querySelector(parent).appendChild(babylonCanvas);
+        babylonCanvas.width = width;
+        babylonCanvas.height = height;
+        babylonCanvas.style.width = width+"px";
+        babylonCanvas.style.height = height+"px";
+        babylonCanvas.style.zIndex = "-1";
+        babylonCanvas.style.position = "absolute";
+        babylonEngine = new babylonx.Engine(babylonCanvas, true);
         ashEngine = new ash.core.Engine();
         ashEngine.addSystem(new SpriteSystem(phaserGame), systemsPriority);
         ashEngine.addSystem(new TextSystem(phaserGame), systemsPriority);
@@ -28,3 +29,4 @@ class Lib {
         ashEngine.addSystem(new FreeCameraSystem(), systemsPriority);
     }
 }
+
