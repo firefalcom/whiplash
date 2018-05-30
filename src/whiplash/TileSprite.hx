8742d01b81ee7c7f7371b88b07d88b331afe5cb4
Gauthier Billot <gauthier.billot@fishingcactus.com>
2018-05-30 10:12:53 +0200

    TileSprites

---
+++ src/whiplash/TileSprite.hx
@@ -0,0 +1,9 @@
+package whiplash;
+
+class TileSprite extends phaser.TileSprite {
+    public function new(w=0, h=0, ?key) {
+        super(Lib.phaserGame, 0, 0, w, h, key);
+        Lib.phaserGame.add.existing(this);
+        untyped this.kill();
+    }
+}
