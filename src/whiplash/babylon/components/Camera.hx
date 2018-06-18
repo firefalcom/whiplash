084bdd9d5d6df2fc9838d0e5572dc471615e657e
Julien Hamaide <julien.hamaide@gmail.com>
2018-06-18 23:45:52 +0200

    Camera rotation was not using a quaternion

--- src/whiplash/babylon/components/Camera.hx
+++ src/whiplash/babylon/components/Camera.hx
@@ -3,6 +3,7 @@ package whiplash.babylon.components;
 class Camera extends SceneObject<BABYLON.TargetCamera> {
     public function new(?o:BABYLON.TargetCamera, scene:BABYLON.Scene) {
         super(o, scene);
+        o.rotationQuaternion = new BABYLON.Quaternion();
         o.getScene().removeCamera(o);
     }
 }
