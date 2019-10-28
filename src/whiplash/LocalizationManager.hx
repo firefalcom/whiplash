6b18eeacedb2adf4f86e20c5c23c5060db522b40
Gauthier Billot <gogoprog@gmail.com>
2019-10-28 11:05:47 +0100

    Updated localization manager

--- src/whiplash/LocalizationManager.hx
+++ src/whiplash/LocalizationManager.hx
@@ -4,10 +4,11 @@ import js.Browser.document;
 
 class LocalizationManager {
     static private var localization:Dynamic;
+    static private var lang:String;
 
-    static public function load(loca:Dynamic) {
+    static public function load(loca:Dynamic, _lang:String) {
         localization = loca;
-
+        lang = _lang;
         apply();
     }
 
@@ -30,6 +31,17 @@ class LocalizationManager {
             var key = untyped e.getAttribute("data-loca");
             untyped e.innerHTML = localization[key];
         }
+    }
+
+    static public function get(key:String) {
+        var value:String = untyped localization[key];
+
+        if(value == null) {
+            trace("Missing translation for '" + key + "'");
+            value = key + "_" + lang;
+        }
+
+        return value;
     }
 }
 
