8059a008a55656afe24ca8d3c5c91358e63fd056
Gauthier Billot <gogoprog@gmail.com>
2020-01-29 13:29:33 +0100

    Added getParameter

--- src/whiplash/Utils.hx
+++ src/whiplash/Utils.hx
@@ -1,5 +1,7 @@
 package whiplash;
 
+import js.Browser.window;
+
 class Utils {
     static public function getReadableTime(seconds:Int):String {
         var secLeft = seconds % 60;
@@ -12,6 +14,12 @@ class Utils {
         }
 
         return minutes + ":" + secString;
+    }
+
+    static public function getParameter(name:String):String {
+        var urlString = window.location.href;
+        var url = new js.html.URL(urlString);
+        return url.searchParams.get(name);
     }
 }
 
