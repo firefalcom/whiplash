3c5c85ecb822d3d641442314c0c12029181eee3f
Gauthier Billot <gogoprog@gmail.com>
2020-08-25 13:33:15 +0200

    Add getRotatedVector

--- src/whiplash/math/Vector2.hx
+++ src/whiplash/math/Vector2.hx
@@ -55,6 +55,13 @@ abstract Vector2(Base) to Base from Base {
         this.y = other.y;
     }
 
+    static public function getRotatedVector(vector:Vector2, angle:Float):Vector2 {
+        var cosinus = Math.cos(angle);
+        var sinus = Math.sin(angle);
+
+        return new Vector2(vector.x * cosinus + vector.y * sinus, -vector.x * sinus + vector.y * cosinus);
+    }
+
 #if babylonjs
     public function setTo(x, y) {
         this.x = x;
