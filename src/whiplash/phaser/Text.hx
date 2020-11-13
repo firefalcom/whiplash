586f152180b8f9d2a40cb15356beff4e0347f7e8
Gauthier Billot <gogoprog@gmail.com>
2020-11-13 14:21:30 +0100

    Haxe4 support (#72)

--- src/whiplash/phaser/Text.hx
+++ src/whiplash/phaser/Text.hx
@@ -2,15 +2,7 @@ package whiplash.phaser;
 
 class Text extends phaser.gameobjects.Text {
     public function new(?text, ?style) {
-        if(style == null) {
-            style = {
-                align: 'center',
-                boundsAlignH: 'left',
-                boundsAlignV: 'top'
-            };
-        }
-
-        super( Lib.phaserScene, 0, 0, text , style);
+        super( Lib.phaserScene, 0, 0, text, style);
         Lib.phaserScene.add.existing(this);
         setActive( false );
         setVisible( false );
