01baa703a047739c5f24fe7b894b82cbe2c49ac1
Gauthier Billot <gogoprog@gmail.com>
2021-04-03 16:53:34 +0200

    Hotfix for ParticleEmitter

--- src/whiplash/phaser/Emitter.hx
+++ src/whiplash/phaser/Emitter.hx
@@ -1,6 +1,8 @@
 package whiplash.phaser;
 
 class Emitter extends phaser.gameobjects.particles.ParticleEmitterManager {
+    public var moveEmitters:Bool = false;
+
     public function new(texture) {
         super(Lib.phaserScene, texture);
         Lib.phaserScene.add.existing(this);
--- src/whiplash/phaser/EmitterSystem.hx
+++ src/whiplash/phaser/EmitterSystem.hx
@@ -34,8 +34,16 @@ class EmitterSystem extends ListIteratingSystem<EmitterNode> {
         var position = transform.position;
         var scale = transform.scale;
         var emitter = node.emitter;
-        emitter.setPosition(position.x, position.y);
-        emitter.setScale(scale.x, scale.y);
-        emitter.setAngle(transform.rotation);
+
+        if(emitter.moveEmitters) {
+            for(name in Reflect.fields(node.emitter.emitters.list)) {
+                var emitter = Reflect.field(node.emitter.emitters.list, name);
+                emitter.setPosition(position.x, position.y);
+            }
+        } else {
+            emitter.setScale(scale.x, scale.y);
+            emitter.setPosition(position.x, position.y);
+            emitter.setAngle(transform.rotation);
+        }
     }
 }
